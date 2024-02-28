452536a82689d8e3e76b20773af374e56e4c7bb5
SsangSoo <kss10080@naver.com>
2024-02-29 00:13:42 +0900

    section 4 OK

--- src/test/java/younghan/querydsl/QuerydslBasicTest.java
+++ src/test/java/younghan/querydsl/QuerydslBasicTest.java
@@ -749,5 +749,37 @@ public class QuerydslBasicTest {
                 .execute();
     }
 
+    @Test
+    @DisplayName("SQL Function1")
+    void sqlFunction() {
+
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        for(String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    @DisplayName("SQL Function2")
+    void sqlFunction2() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower', {0})", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for(String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
 
 }
